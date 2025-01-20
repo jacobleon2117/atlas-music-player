aafa1aa67205af9da26974c03ddd7423b4bd7a37
jacobleon2117 <jacobleon2117@gmail.com>
2025-01-20 12:00:52 -0600

    Changed File: CoverArt.jsx to change the rounded edges to sm

--- src/components/CoverArt.jsx
+++ src/components/CoverArt.jsx
@@ -3,7 +3,8 @@ export default function CoverArt() {
     <img
       src="/placeholder.svg"
       alt="Album Cover"
-      className="h-96 w-96 rounded-lg"
+      aria-label="Album artwork for current song"
+      className="h-96 w-96 rounded-sm"
     />
   );
 }
