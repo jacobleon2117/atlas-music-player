ded5d6b0eef4f21de9d6f8741037ad279478f7f3
jacobleon2117 <jacobleon2117@gmail.com>
2025-01-20 13:41:07 -0600

    Changed Files: App.jsx to import the file Playlist.jsx for styling and Removed PlayListItem.jsx from CurrentlyPlaying.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
 import Footer from "./Footer";
-import CurrentlyPlaying from "./CurrentlyPlaying";
+import Playlist from "./Playlist";
 
 function App() {
   return (
     <div className="flex min-h-screen flex-col justify-between bg-white p-8">
       <div className="flex flex-col items-center gap-8">
-        <CurrentlyPlaying />
+        <Playlist />
       </div>
       <Footer />
     </div>
--- src/components/CurrentlyPlaying.jsx
+++ src/components/CurrentlyPlaying.jsx
@@ -2,7 +2,6 @@ import CoverArt from "./CoverArt";
 import SongTitle from "./SongTitle";
 import PlayControls from "./PlayControls";
 import VolumeControls from "./VolumeControls";
-import PlayListItem from "./PlayListItem";
 
 export default function MusicPlayer() {
   return (
