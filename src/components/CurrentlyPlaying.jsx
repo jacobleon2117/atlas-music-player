936b198976945e4c7edb3800d744217289b774bf
jacobleon2117 <jacobleon2117@gmail.com>
2025-01-20 13:31:10 -0600

    Added test code from last task to CurrentlyPlaying.jsx

---
+++ src/components/CurrentlyPlaying.jsx
@@ -0,0 +1,27 @@
+import CoverArt from "./CoverArt";
+import SongTitle from "./SongTitle";
+import PlayControls from "./PlayControls";
+import VolumeControls from "./VolumeControls";
+import PlayListItem from "./PlayListItem";
+
+export default function MusicPlayer() {
+  return (
+    <div className="flex w-[400px] flex-col">
+      <div className="h-[400px] w-[400px]">
+        <CoverArt />
+      </div>
+      <div className="mt-6 h-[62px] w-full">
+        <SongTitle />
+      </div>
+      <div className="mt-6 h-[62px] w-full">
+        <PlayControls />
+      </div>
+      <div className="mt-6 h-[18px] w-full">
+        <VolumeControls />
+      </div>
+      <div className="mt-6 w-full">
+        <PlayListItem />
+      </div>
+    </div>
+  );
+}
